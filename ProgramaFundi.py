862b396fda061f5e8f18a37b59496927bc0a1878
Mine-J <jsaezg@alumnos.nebrija.es>
2025-12-15 15:32:21 +0100

    Actualizar horarios de clases en la lista de CLASES para mejorar la programación semanal.

--- ProgramaFundi.py
+++ ProgramaFundi.py
@@ -37,11 +37,12 @@ CLASES = [
     {"dia": "lunes", "hora": "17:00", "nombre": "Pilates MesD"},
     {"dia": "lunes", "hora": "18:00", "nombre": "Entrenamiento en suspensión"},
     {"dia": "martes", "hora": "15:45", "nombre": "Fuerza en sala multitrabajo"},
-    {"dia": "martes", "hora": "18:00", "nombre": "Pilates MesD"},
+    {"dia": "martes", "hora": "17:00", "nombre": "Entrenamiento Funcional"},
     {"dia": "miércoles", "hora": "15:45", "nombre": "Fuerza GAP"},
     {"dia": "miércoles", "hora": "17:00", "nombre": "Pilates MesD"},
     {"dia": "miércoles", "hora": "18:00", "nombre": "Entrenamiento en suspensión"},
     {"dia": "jueves", "hora": "15:45", "nombre": "Fuerza en sala multitrabajo"},
+    {"dia": "jueves", "hora": "17:00", "nombre": "Entrenamiento Funcional"},
     {"dia": "viernes", "hora": "15:45", "nombre": "Pilates MesD"},
     {"dia": "viernes", "hora": "17:00", "nombre": "Entrenamiento Funcional"}
 ]
